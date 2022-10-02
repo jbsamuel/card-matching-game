f0ae7b76dcfc9ea85432ce3fa9c93aad442540e5
jbsamuel <22019376+jbsamuel@users.noreply.github.com>
2022-10-02 02:00:57 -0400

    Added some exception handling. Need to handle incorrect input error for integer while picking a card

--- card_match.py
+++ card_match.py
@@ -1,6 +1,12 @@
 cards = ["Ace", "King", "Queen"]
 
 user_card_choice = str(input("Choose a card: King. Queen, or Ace\n"))
+user_card_choice = user_card_choice.lower()
+while user_card_choice == "":
+    print("Please enter a choice\n")
+    user_card_choice = str(input("Choose a card: King. Queen, or Ace\n"))
+    user_card_choice = user_card_choice.lower()
+
 match user_card_choice:
     case "Ace" | "ace":
         user_card_choice = cards[0]
@@ -9,9 +15,8 @@ match user_card_choice:
     case "Queen" | "queen":
         user_card_choice = cards[2]
     case _:
-        print("Not a valid card choice")
+        print("Please enter a valid card choice\n")
         quit()
-
 
 pick_card_number = int(input("Pick a card: 1-3\n"))
 match pick_card_number:
@@ -22,7 +27,7 @@ match pick_card_number:
     case 3:
         pick_card_number = cards[2]
     case _:
-        print("Not a valid number choice")
+        print("Please enter a valid number")
         quit()
 
 if user_card_choice == pick_card_number:
