06fc68f273c7f0ba96ecc7844932443fc5fe94a4
jbsamuel <22019376+jbsamuel@users.noreply.github.com>
2022-10-01 20:42:18 -0400

    Testing user inputs for picking a card number

--- card_match.py
+++ card_match.py
@@ -1,13 +1,13 @@
-# cards = None
 # cards_ace = ["Ace"]
 # cards_queen = ["Queen"]
 # cards_king =["King"]
+cards = [["Ace"], ["King"], ["Queen"]]
 user_card_choice = str(input("Choose a card: King. Queen, or Ace\n"))
 user_card_choice = user_card_choice.lower()
-match user_card_choice:
-    case "ace" | "queen" | "king":
+pick_card_number = int(input("Pick a card: 0-2\n"))
+match pick_card_number:
+    case 0 | 1 | 2:
         print("it works")
-        print(user_card_choice)
+        print(pick_card_number)
     case _:
         print("not working")
-        user_card_choice = str(input("Choose a card: King. Queen, or Ace\n"))
